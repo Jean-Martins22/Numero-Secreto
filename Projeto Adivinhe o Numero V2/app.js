c40dc01850c894e7a1e93b304579f8ae5ade3244
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-08-15 14:42:34 -0300

    Criação de funções

--- Projeto Adivinhe o Numero V2/app.js
+++ Projeto Adivinhe o Numero V2/app.js
@@ -1,9 +1,18 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
-
 function verificarChute(){
-    console.log('O ovo foi comido po');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+
+let numeroSecreto = gerarNumeroAleatorio();
