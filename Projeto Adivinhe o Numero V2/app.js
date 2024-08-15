c72a0bb02664aa1185ad56bbe57b9001dc3eb6e4
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-08-15 12:41:34 -0300

    Criação de regras para o jogo

---
+++ Projeto Adivinhe o Numero V2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+
+function verificarChute(){
+    console.log('O ovo foi comido po');
+}
